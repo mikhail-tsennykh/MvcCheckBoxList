757c1205be7fd9026fbbbe3c6cd1fedb03ac800c
Mikhail T <siam.nanaah@gmail.com>
2012-02-06 15:52:23 -0600

    mvc sample app creation

---
+++ MvcCheckBoxListSampleApp/Controllers/HomeController.cs
@@ -0,0 +1,11 @@
+﻿using System.Web.Mvc;
+
+namespace MvcCheckBoxListSampleApp.Controllers {
+	public class HomeController : Controller {
+
+		public ActionResult Index() {
+			return View();
+		}
+
+	}
+}
