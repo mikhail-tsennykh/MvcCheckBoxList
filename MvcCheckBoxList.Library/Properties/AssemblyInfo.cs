8087974f66e4c7d16b383ed15e989e23027c1cd7
Mikhail T <siam.nanaah@gmail.com>
2012-02-20 16:50:23 -0600

    converted MvcCheckBoxList.cs to library (*.dll file)

---
+++ MvcCheckBoxList.Library/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MvcCheckBoxList.Library")]
+[assembly: AssemblyDescription("MVC HtmlHelper class extension which creates a custom " +
+                               "POSTable checkbox list.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Mikhail Tsennykh")]
+[assembly: AssemblyProduct("@Html.CheckBoxList() custom extension")]
+[assembly: AssemblyCopyright("Copyright © 2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("9f25ec2b-374b-4f0a-b1a6-bc8c6ab3eaed")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.4.2.0")]
+[assembly: AssemblyFileVersion("1.4.2.0")]
