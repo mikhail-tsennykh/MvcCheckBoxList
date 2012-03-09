89da997274d97df9b32bb8e3f514a9a8aa4e5966
Mikhail T <siam.nanaah@gmail.com>
2012-03-09 15:11:54 -0600

    Fixed a bug where control requires System.Web.WebPages 2.0.0.0 and fails to work in MVC3 projects

--- MvcCheckBoxList.Library/Properties/AssemblyInfo.cs
+++ MvcCheckBoxList.Library/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Mikhail Tsennykh")]
 [assembly: AssemblyProduct("@Html.CheckBoxList() custom extension")]
-[assembly: AssemblyCopyright("Copyright © 2012")]
+[assembly: AssemblyCopyright("Copyright © 2012 Mikhail Tsennykh")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.4.2.2")]
-[assembly: AssemblyFileVersion("1.4.2.2")]
+[assembly: AssemblyVersion("1.4.2.3")]
+[assembly: AssemblyFileVersion("1.4.2.3")]
