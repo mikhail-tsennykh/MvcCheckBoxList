fe60b0ca98c69287e144d56f133a52793c365805
Mikhail Tsennykh <siam.nanaah@gmail.com>
2012-07-30 10:48:48 -0500

    updated sln file

--- MvcCheckBoxListSampleApp/Controllers/HomeController.cs
+++ MvcCheckBoxListSampleApp/Controllers/HomeController.cs
@@ -6,12 +6,26 @@ using MvcCheckBoxListSampleApp.ViewModels;
 
 namespace MvcCheckBoxListSampleApp.Controllers {
 	public class HomeController : Controller {
+	  public HomeController() {
+	    ViewBag.CodePrejectUrl = "http://www.codeproject.com/Articles/292050/MVC3-Html-CheckBoxList-custom-extension";
+	    ViewBag.GitHubZipSourceUrl = "https://github.com/mikhail-tsennykh/MVC3-Html.CheckBoxList-custom-extension/zipball/master";
+	    ViewBag.GitHubUrl = "https://github.com/mikhail-tsennykh/MVC3-Html.CheckBoxList-custom-extension";
+	  }
 
-    public ActionResult Index() {
-      return RedirectToAction("Examples");
+    public ActionResult Index(string[] cities, PostedCities postedCities) {
+      ViewBag.HideMenu = true;
+      return View(GetCitiesModel(cities, postedCities));
     }
 
 		public ActionResult Examples(string[] cities, PostedCities postedCities) {
+		  return View(GetCitiesModel(cities, postedCities));
+		}
+
+    public ActionResult Docs() {
+      return View();
+    }
+    
+    private CitiesViewModel GetCitiesModel(string[] cities, PostedCities postedCities) {
 			// setup properties
 			var model = new CitiesViewModel();
 			var selectedCities = new List<City>();
@@ -40,32 +54,8 @@ namespace MvcCheckBoxListSampleApp.Controllers {
 			model.SelectedCities = selectedCities;
 			model.PostedCities = postedCities;
 
-			return View(model);
-		}
-
-    public ActionResult Docs() {
-      return View();
+      return model;
     }
-
-    //public ActionResult SelectListBased(string[] cities) {
-    //  // setup properties
-    //  var model = new CitiesViewModel();
-    //  var selectedCities = new List<City>();
-
-    //  // if an array of posted city ids exists and is not empty,
-    //  // save selected ids
-    //  if (cities != null && cities.Any()) {
-    //    selectedCities = CityRepository.GetAll()
-    //      .Where(x => cities.Any(s => x.Id.ToString().Equals(s))).ToList();
-    //    model.WasPosted = true;
-    //  }
-
-    //  // setup a view model
-    //  //model.AvailableCities = CityRepository.GetAll();
-    //  model.SelectedCities = selectedCities;
-
-    //  return View(model);
-    //}
 
 	}
 }
